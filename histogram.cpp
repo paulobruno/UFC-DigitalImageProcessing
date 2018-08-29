b8f2e8f1a600582548119753dd4bc5c27969eeaa
felipeLucio1 <felipelucio2009@gmail.com>
2018-08-29 14:12:48 -0300

    negative filter

--- histogram.cpp
+++ histogram.cpp
@@ -1,15 +1,41 @@
 #include "histogram.h"
 
+#define T_MAX 255
+
 
 void showHistogram(const cv::Mat& pHist, cv::Mat& pDst, const unsigned int pWidth, const unsigned int pHeight)
 {
 	cv::Mat img = cv::Mat::zeros(pWidth, pHeight, CV_8U);
 
-	
+
 }
 
 
 void calcHistogram(const cv::Mat& pSrc, cv::Mat& pDst)
 {
-	
+	// Entrada: pSrc - matriz da imagem;
+	//			pDst - vetor do histograma.
+
+	pDst = cv::Mat::zeros(1, 256, CV_8U);
+
+	for (int i = 0; i < pSrc.rows; i++) {
+		for (int j = 0; j < pSrc.cols; j++) {
+			pDst.at<uchar>(0 ,pSrc.at<uchar>(i, j)) += 1;
+		}
+	}
+}
+
+
+
+void negativeFIlter(const cv::Mat& pSrc, cv::Mat& pDst)
+{
+	pDst = cv::Mat::zeros(pSrc.rows, pSrc.cols, CV_8U);
+
+	for (unsigned int i = 0; i < pSrc.rows; ++i) 
+	{
+		for (unsigned int j = 0; j < pSrc.cols; ++j) 
+		{
+			pDst.at<uchar>(i, j) = T_MAX - pSrc.at<uchar>(i, j);
+		}
+	}
 }
--- histogram.h
+++ histogram.h
@@ -6,3 +6,5 @@
 void showHistogram(const cv::Mat& pHist, cv::Mat& pDst, const unsigned int pWidth, const unsigned int pHeight);
 
 void calcHistogram(const cv::Mat& pSrc, cv::Mat& pDst);
+
+void negativeFIlter(const cv::Mat& pSrc, cv::Mat& pDst);
