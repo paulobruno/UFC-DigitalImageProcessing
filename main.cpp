e48c013251ec49c72ddc4af36404c4e12f826fa9
felipeLucio1 <felipelucio2009@gmail.com>
2018-08-31 11:26:17 -0300

    ajustes de ultima hora

--- main.cpp
+++ main.cpp
@@ -24,48 +24,49 @@ int main(int argc, const char** argv)
 		return -1;
 	}
 
-	//cv::imshow(filename, img);
+
+	cv::imshow(filename, img); // Imagem original
 
 	cv::Mat hist;
 	calcHistogram(img, hist);
 
 	cv::Mat histImg;
 	showHistogram(hist, histImg, 512, 400);
-	//cv::imshow("histImg", histImg);
+	cv::imshow("histImg", histImg); // Histograma
 
 	cv::Mat eqImg;
 	equalizeHistogram(img, eqImg);
-	//cv::imshow("eq_hist.jpg", eqImg);
+	cv::imshow("eq_hist", eqImg); // Imagem equalizada
 
 	cv::Mat eqHist;
 	calcHistogram(eqImg, eqHist);
 
 	cv::Mat eqhimg;
 	showHistogram(eqHist, eqhimg, 512, 400);
-	//cv::imshow("eq", eqhimg);
+	cv::imshow("eq", eqhimg); // Histograma equalizado
 
 	cv::Mat negative;
 	negativeFilter(img, negative);
-	//cv::imshow("neg", negative);
+	//cv::imshow("neg", negative); // Filtro negativo
 
 	cv::Mat logImg;
 	logarithmicFilter(img, logImg, 50);
-	//cv::imshow("log", logImg);
+	//cv::imshow("log", logImg); // FIltro logaritmico
 
 	cv::Mat gama;
 	powerFilter(img, gama, 1, 0.8f);
-	//cv::imshow("gama", gama);
+	//cv::imshow("gama", gama); // Correcao gama
 
 	cv::Mat linear;
 	linearParts(img, 100, 180, 150, 120, linear);
-	//cv::imshow("linear", linear);
+	//cv::imshow("linear", linear); // Linear por partes
 
     std::vector<cv::Mat> slices;
     bitPlaneSlice(img, slices);
 
     for (unsigned int i = 0; i < slices.size(); ++i)
     {
-        cv::imshow("slice" + std::to_string(i), slices.at(i));
+        //cv::imshow("slice" + std::to_string(i), slices.at(i)); // Camada de bits
     }
 
 	cv::waitKey();
