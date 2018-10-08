c3b00e8d3c1095355a13569427212d9f80b9ce62
Paulo Bruno <pbrunosousa@gmail.com>
2018-10-08 10:53:29 -0300

    Sepia filter, contrast and brightness

--- colorMain.cpp
+++ colorMain.cpp
@@ -16,7 +16,7 @@ int main(int argc, const char** argv)
 
 	std::string filename(argv[1]);
 
-	cv::Mat img = cv::imread(filename, cv::IMREAD_GRAYSCALE);
+	cv::Mat img = cv::imread(filename, cv::IMREAD_COLOR);
 
 	if (img.empty())
 	{
@@ -36,8 +36,35 @@ int main(int argc, const char** argv)
     cv::Mat sepiaImg;
     sepiaFilter(img, sepiaImg);
     cv::imshow("Sepia", sepiaImg);
+    
+    cv::Mat matA = cv::imread("soccer.jpg", cv::IMREAD_COLOR);
+    cv::Mat matB = cv::imread("americanFootball.jpg", cv::IMREAD_COLOR);    
+    cv::Mat subtractImg;
+    imageSubtraction(matA, matB, subtractImg);
+    cv::imshow("Subtraction", subtractImg);
+    
+    cv::Mat threshImg;
+    thresholding(img, threshImg, 80);
+    cv::imshow("Threshold", threshImg);
+    
+    cv::Mat chromaImg = cv::imread("chroma_key.jpg", cv::IMREAD_COLOR);
+    cv::Vec3b key = {90, 160, 170};
+    cv::Mat keyedMat;
+    chromaKeying(chromaImg, keyedMat, key, 20);
+    cv::imshow("Chroma Key", keyedMat);
+    
+    cv::Mat brightMat;
+    adjustBrightness(img, brightMat, 50);
+    cv::imshow("Bright", brightMat);
 
+    cv::Mat contrastMat;
+    adjustContrast(img, contrastMat, 0.5f);
+    cv::imshow("Contrast", contrastMat);
 
+    cv::Mat brightContrastMat;
+    adjustBrightAndContrast(img, brightContrastMat, 0.5f, 50);
+    cv::imshow("Bright and Contrast", brightContrastMat);
+    
 	cv::waitKey();
 	
 
--- coloring.h
+++ coloring.h
@@ -16,10 +16,14 @@ void hsiToCmy(const cv::Mat& pSrc, cv::Mat& pDst);
 void sepiaFilter(const cv::Mat& pSrc, cv::Mat& pDst);
 
 
-void chromaKeying(const cv::Mat& pSrc, cv::Mat& pDst, const cv::Vec3b pColorKey, unsigned int pEpsilon = 30);
+void chromaKeying(const cv::Mat& pSrc, cv::Mat& pDst, const cv::Vec3b pColorKey, unsigned char pEpsilon = 20);
 
 void imageSubtraction(const cv::Mat& pSrc1, const cv::Mat& pSrc2, cv::Mat& pDst);
 
-void thresholding(const cv::Mat& pSrc, cv::Mat& pDst);
+void thresholding(const cv::Mat& pSrc, cv::Mat& pDst, const uchar pThreshold);
 
-void adjustBrightness(const cv::Mat& pSrc, cv::Mat& pDst);
+void adjustBrightness(const cv::Mat& pSrc, cv::Mat& pDst, const uchar pBrightness);
+
+void adjustContrast(const cv::Mat& pSrc, cv::Mat& pDst, const float pGain);
+
+void adjustBrightAndContrast(const cv::Mat& pSrc, cv::Mat& pDst, const float pGain, const uchar pBrightness);
